9d1de83a5145505ab1aa0d7a141a8c56d775efc9
KyroC <Kyroeth@gmail.com>
2023-05-30 16:49:11 +0800

    Set up + route to  /bugs

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bugRouter = require('./controllers/bugController')
+
+const DB = process.env.ATLAS_URI
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log(`Database connected..`)
+})
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/bugs',bugRouter)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})

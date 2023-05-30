dcb6bcdfa86496bb491e0c83bf898a673b2acd2b
KyroC <Kyroeth@gmail.com>
2023-05-30 16:51:29 +0800

    Set up

---
+++ server/models/projectModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    users: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    bugs: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Bug"
+    }
+})
+
+const Project = mongoose.model('Project',projectSchema)
+
+module.exports = Project;

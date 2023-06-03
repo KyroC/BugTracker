83b6a6b5366c3759e015d3ddf52d4e855b95b55a
KyroC <Kyroeth@gmail.com>
2023-06-03 20:44:08 +0800

    Added jwt for user login

---
+++ server/controllers/loginController.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const loginRouter = require('express').Router()
+const User = require('../models/userModel')
+
+loginRouter.post('/', async(req,res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+    //check if user exists
+    const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+        return res.status(401).json({
+            error: 'invalid username or password'
+        })
+    }
+    const userForToken = {
+        email: user.email,
+        id: user._id
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    res
+        .status(200)
+        .send({token, user: user.name})
+})
+
+module.exports = loginRouter

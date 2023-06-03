c80d992cacb224f3b29987de7bc5b71d0912920c
KyroC <Kyroeth@gmail.com>
2023-06-03 12:28:39 +0800

    Added sample data + bugcard import

--- client/src/pages/projects.js
+++ client/src/pages/projects.js
@@ -1,11 +1,54 @@
 import "./projects.css"
+import Bugcard from "../components/Bugcard"
 
 let statuses = ["To Do", "In Progress", "To Review", "Completed"]
-
+let f_bug=[]
+//sample data
+let bugs = [{
+    status: "To Do",
+    title: "bug1",
+    author: "author1",
+    description: "description1",
+    team: "UI",
+    comment: 5,
+},
+{
+    status: "In Progress",
+    title: "bug2",
+    author: "author2",
+    description: "description2",
+    team: "UI",
+    comment: 4,
+},
+{
+    status: "To Review",
+    title: "bug3",
+    author: "author3",
+    description: "description3",
+    team: "UI",
+    comment: 5,
+},
+{
+    status: "Completed",
+    title: "bug4",
+    author: "author4",
+    description: "description4",
+    team: "UI",
+    comment: 5,
+},
+{
+    status: "To Do",
+    title: "bug5",
+    author: "author5",
+    description: "description5",
+    team: "UI",
+    comment: 5,
+}]
 const ProjectDisplay = () => {
     return(statuses.map(status => 
-    <div className="project-card">
-        {status}
+    <div className="bug-card">
+        <div className="bug-status"><h2>{status}</h2></div> 
+        <Bugcard bugs={bugs.filter(bug => bug.status === status)} />
     </div>)
     )
 }
@@ -13,10 +56,8 @@ const ProjectDisplay = () => {
 
 const Projects = () => {
     return(
-        <div className="projects">
-            <div className="projects-container">
-                <ProjectDisplay />
-            </div>
+        <div className="bug-grid">
+            <ProjectDisplay />
         </div>
     )
 }

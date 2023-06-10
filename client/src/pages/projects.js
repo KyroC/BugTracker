383f13c2d62a286dec6e2458ef9ba8a68b018e34
KyroC <Kyroeth@gmail.com>
2023-06-10 14:25:38 +0800

    + Axios get request

--- client/src/pages/projects.js
+++ client/src/pages/projects.js
@@ -1,4 +1,15 @@
+import { useState, useEffect } from 'react'
+import projectService from '../services/projectService'
+
 const Projects = () => {
+    const [projectsArray, setProjectsArray] = useState([])
+    useEffect(() => {
+        projectService.getAll()
+        .then(res => {
+            setProjectsArray(res)
+        })
+    })    
+
     return (
         <div className="projects-page">
             Project Details
@@ -6,7 +17,13 @@ const Projects = () => {
                 <button>New Project</button>
             </div>
             <div className="projects-container">
-                
+                <div className="project-container-title"></div>
+                <div className="project-list">
+                    {
+                        projectsArray.map(project => 
+                            <li>{project.name}</li>)
+                    }
+                </div>
             </div>
         </div>
     )

f9ef4b60ee9e8f18406a2f1a675986f252d6884c
KyroC <Kyroeth@gmail.com>
2023-07-01 23:56:20 +0800

    Edit page

--- client/src/pages/projectEdit.js
+++ client/src/pages/projectEdit.js
@@ -6,9 +6,19 @@ import './projectEdit.css'
 let projects = (id) => {
     return projectService.getProject(id) 
 }
+let users = () => {
+    return projectService.getUser()
+}
 const ProjectEdit = () => {
     const [projectArray, setProjectArray] = useState([])
+    const [userArray, setUserArray] = useState([])
     const { id } = useParams()
+    useEffect(() => {
+        users()
+        .then(res => {console.log(res)
+            setUserArray(res)}
+            )
+    },[])
     useEffect(() => {
         projects(id)
         .then(res => {
@@ -21,8 +31,24 @@ const ProjectEdit = () => {
         <div>
             <div className="project-edit-title">Edit Project</div>
             <div>
-                Assign ProjectArray
-                <div>Current Assigned ProjectArray</div>
+                <div>Assign Users</div>
+                <div className="all-user-details">
+                    <form action="#">
+                        <select name="users" id="users" className="all-user-details-dropdown">
+                        {userArray?.map((user) => (
+                                <option className="all-user-details-name">{user.name}</option>
+                        ))}
+                        </select>
+                        <select name="roles" id="roles" className="all-user-roles-dropdown">
+                            <option>Developer</option>
+                            <option>UI/UX</option>
+                            <option>Admin</option>
+                        </select>
+                        <input type="submit" value="submit" />
+                    </form>
+                </div>
+
+                <div>Current Assigned</div>
                 {projectArray.users?.map((user)=> ( 
                     <div className="assigned-user-details">
                         <div className="assigned-user-details-data">{user.name}</div>
@@ -31,8 +57,6 @@ const ProjectEdit = () => {
                     </div>
                  ))}
                 
-            </div>
-            <div>
             </div>
         </div>
     )

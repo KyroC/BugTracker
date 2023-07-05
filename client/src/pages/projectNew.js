e1d975b5fed1dc57156c0304b7c1f790c54d8564
KyroC <Kyroeth@gmail.com>
2023-07-05 14:27:52 +0800

    Created New Project page

---
+++ client/src/pages/projectNew.js
@@ -0,0 +1,11 @@
+import {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+
+const ProjectNew = () => {
+        return(
+            <div>
+                <h1>Create new project</h1>
+            </div>
+        )
+}
+export default ProjectNew

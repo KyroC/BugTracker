d85ed3d194cfeb0a554562257ab394eeea53a57c
KyroC <Kyroeth@gmail.com>
2023-07-10 23:57:40 +0800

    updated

--- client/src/pages/addTicket.js
+++ client/src/pages/addTicket.js
@@ -7,6 +7,35 @@ let projects = (id) => {
 }
 
 const AddTicket = () => {
+    const [ticketName, setTicketName] = useState("")
+    const [ticketDetails, setTicketDetails] = useState("")
+    const [ticketPriority, setTicketPriority] = useState("")
+    const [ticketStatus, setTicketStatus] = useState("")
+
+    const handleTicketName = (event) => {
+        event.preventDefault()
+        setTicketName(event.target.value)
+    }
+    const handleTicketDetails = (event) => {
+        event.preventDefault()
+        setTicketDetails(event.target.value)
+    }
+    const handleTicketPriority = (event) => {
+        event.preventDefault()
+        setTicketPriority(event.target.value)
+    }
+    const handleTicketStatus = (event) => {
+        event.preventDefault()
+        setTicketPriority(event.target.value)
+    }
+    const handleTicketSubmit = () => {
+        let ticketObj = {
+            name: ticketName,
+            details: ticketDetails,
+            priority: ticketPriority,
+            status: ticketStatus 
+        }
+    }
 
     return (
         <div className="add-ticket-page">
@@ -14,25 +43,26 @@ const AddTicket = () => {
             <div className="add-ticket-page-table">
                 <form>
                     <label> Name:
-                    <input type="text" value="" name="Name"></input><br />
+                        <input type="text" value={ticketName} name="Name" onChange={handleTicketName} /><br />
                     </label>
                     <label> Detail
-                        <input type="text" value="" name="Detail"></input><br />
+                        <input type="text" value={ticketDetails} name="Detail" onChange={handleTicketDetails} /><br />
                     </label>
                     <label>Priority:
-                        <select> 
+                        <select onChange={handleTicketPriority}> 
                             <option value="High"> High </option>
                             <option value="Medium"> Medium </option>
                             <option value="low"> Low </option>
                         </select> <br />
                     </label>
                     <label> Status:
-                        <select>
+                        <select onChange={handleTicketStatus}>
                             <option value="Open">Open</option>
                             <option value="Worked on">Worked on</option>
                             <option value="Solved">Solved</option>
-                        </select>
+                        </select> < br />
                     </label>
+                    <button onClick={handleTicketSubmit}>Create Ticket</button>
                 </form>
             </div>
         </div>

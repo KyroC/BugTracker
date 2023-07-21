2f07799acd5f9ac67899bbe90e4cc2c2542c9167
KyroC <Kyroeth@gmail.com>
2023-07-21 16:54:39 +0800

    Created bugDetails page for individual bug details

---
+++ client/src/pages/bugDetails.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+
+const BugDetail = () => {
+    return (
+        <div>
+            Ticket Details
+            <div>
+                Div 1
+            </div>
+            <div>
+                Div 2
+            </div>
+            <div>
+                Div 3
+            </div>
+        </div>
+    )
+}
+
+export default BugDetail;

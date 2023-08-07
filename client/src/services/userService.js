a381eb598c747bcb548732538067b8e9cbafc778
KyroC <Kyroeth@gmail.com>
2023-08-07 14:50:42 +0800

    Added setToken function + config to getUserProjects service

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -2,11 +2,19 @@ import axios from 'axios';
 const usersUrl = "/api/users"
 const bugsUrl = "/api/bugs"
 
-const getUserProjects = (id) => {
-    const request = axios.get(usersUrl + "/" + id + "/projects")
-    console.log(id)
+let token = null
+
+const setToken = newToken => {
+    token = `Bearer ${newToken}`
+}
+
+const getUserProjects = () => {
+    const config = {
+        headers: { Authorization: token},
+    }
+    const request = axios.get(usersUrl + "/projects", config)
     return request.then(response => response.data)
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {getUserProjects, }
+export default {getUserProjects, setToken}

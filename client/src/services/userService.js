6c271c9e6f8a06d7f52b9e003e8e726c6242859e
KyroC <Kyroeth@gmail.com>
2023-08-07 18:04:31 +0800

    Updated code to add authorization + setting token

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -10,11 +10,18 @@ const setToken = newToken => {
 
 const getUserProjects = () => {
     const config = {
-        headers: { Authorization: token},
+        headers: { Authorization: token}
     }
     const request = axios.get(usersUrl + "/projects", config)
     return request.then(response => response.data)
 }
+const getUserBugs = () => {
+    const config = {
+        headers: { Authorization: token}
+    }
+    const request= axios.get(usersUrl + "/tickets", config)
+    return request.then(response => response.data)
+}
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {getUserProjects, setToken}
+export default {getUserProjects, setToken, getUserBugs}

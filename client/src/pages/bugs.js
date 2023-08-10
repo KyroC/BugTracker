68c190afd167c7c609f6a3be6e2baee73e33f94b
KyroC <Kyroeth@gmail.com>
2023-08-10 23:37:18 +0800

    Added sort

--- client/src/pages/bugs.js
+++ client/src/pages/bugs.js
@@ -6,17 +6,38 @@ import styles from "./bugs.module.css"
 const bugsList = () => {
     return userService.getUserBugs()
 }
+
 const Bugs = () => {
     const [bugsArray, setBugsArray] = useState([])
     const [filteredArray, setFilteredArray] = useState([])
+    const [sortedArray, setSortedArray] = useState([])
+    const sortByHighPriority = 
+        //to double check
+        sortedArray.sort((a,b) => {
+            if (a.priority === "High" && b.priority!=="High") {
+                return -1
+            }
+            if (a.priority === "Medium" && b.priority!=="Medium") {
+                return -1
+            }
+            return 0
+        }
 
+        ) 
+    const handlePriorityClick = () => {
+        setSortedArray(sortByHighPriority)
+        console.log(sortByHighPriority)
+        console.log(sortedArray)
+    }
     useEffect(() => {
         bugsList()
         .then(res => {
             setBugsArray(res)
+            console.log(bugsArray)
             setFilteredArray(res)
+            setSortedArray(res)
         })
-    })
+    },[])
     return(
         <div className={styles.bugs}>
             <div className={styles.bugsTable}>
@@ -27,7 +48,7 @@ const Bugs = () => {
                     <div className={styles.bugData}><b>Type</b></div>
                     <div className={styles.bugData}><b>Priority</b></div>
                 </div>
-                {bugsArray.map(bug => (
+                {sortedArray.map(bug => (
                     <div className={styles.bugsTableRow}>
                         <div className={styles.bugData}>{bug.name}</div>
                         <div className={styles.bugData}>{bug.detail}</div>
@@ -36,6 +57,7 @@ const Bugs = () => {
                         <div className={styles.bugData}>{bug.priority}</div>
                     </div>
                  ))}
+                 <button onClick={ handlePriorityClick}>Sort</button>
             </div>
         </div>
     )

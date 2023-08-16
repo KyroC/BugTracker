52674b616cd3e0625c9e64b16228913300fb9bfc
KyroC <Kyroeth@gmail.com>
2023-08-16 21:45:52 +0800

    Removed app.get

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const bugRouter = require('./controllers/bugController')
 const usersRouter = require('./controllers/userController')
 const projectRouter = require('./controllers/projectController')
 const loginRouter = require('./controllers/loginController')
-const path = require('path');
+
 require("express-async-errors")
 
 const DB = process.env.ATLAS_URI
@@ -22,13 +22,7 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+
 
 //Routes
 app.use('/api/bugs', bugRouter)

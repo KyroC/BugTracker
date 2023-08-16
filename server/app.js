89f2849b4943a33adad4d2ee5cd2f4f38bea1aa0
KyroC <Kyroeth@gmail.com>
2023-08-17 00:37:38 +0800

    Removed app.get

--- server/app.js
+++ server/app.js
@@ -22,9 +22,6 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 //Routes

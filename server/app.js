894e4e365603b5f7f714002a747d0eee161096b7
KyroC <Kyroeth@gmail.com>
2023-08-16 21:38:23 +0800

    Updated path to index.html
    Previous save did not add the updated path

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(cors())
 app.use(express.static('build'))
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }

35570e0cb8e05c6a926dfcb0abd22ddfc312ff36
KyroC <Kyroeth@gmail.com>
2023-08-17 00:49:42 +0800

    uibuild

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const bugRouter = require('./controllers/bugController')
 const usersRouter = require('./controllers/userController')
 const projectRouter = require('./controllers/projectController')
 const loginRouter = require('./controllers/loginController')
+const path = require('path');
 require("express-async-errors")
 
 const DB = process.env.ATLAS_URI
@@ -21,6 +22,9 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 //Routes

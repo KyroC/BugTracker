424968cacdb6c5584f0152375ae74d782234dd72
KyroC <Kyroeth@gmail.com>
2023-08-19 13:20:51 +0800

    Added functionality to display logged in username
    Added useEffect on render to include getUser, which is displayed on topbar

--- client/src/components/Topbar.js
+++ client/src/components/Topbar.js
@@ -1,14 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import {useEffect, useState} from 'react';
 import './Topbar.css';
+import userService from '../services/userService';
+
 const onLogoutClick = () => {
     localStorage.clear()
 }
+const userProfile = () => {
+    return userService.getUser()
+}
 
 const Topbar = () => {
-   
+    const [user,setUser] = useState([])
+    useEffect(() => {
+        userProfile()
+        .then(res => {
+            setUser(res)
+            console.log(res)
+        })
+    },[])
     return(
         <div className="topbar">
-            <div className="topbar-left">Logged in as: test</div>
+            <div className="topbar-left">Logged in as: {user.name}</div>
             <div className="topbar-right">
                 <ul>
                     <li>Search</li>

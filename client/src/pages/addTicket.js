90b171879207da4790e02d82c60dcb2811404195
KyroC <Kyroeth@gmail.com>
2023-08-20 17:49:35 +0800

    imported css module + wrapped div in a container for styling
    new css module - addTicket.module.css created and imported for styling

--- client/src/pages/addTicket.js
+++ client/src/pages/addTicket.js
@@ -2,6 +2,7 @@ import { useState} from 'react'
 import { useParams, Link } from 'react-router-dom'
 import projectService from '../services/projectService.js'
 import bugService from '../services/bugService'
+import styles from './addTicket.module.css'
 
 const AddTicket = () => {
     const [ticketName, setTicketName] = useState("")
@@ -41,34 +42,36 @@ const AddTicket = () => {
     }
 
     return (
-        <div className="add-ticket-page">
-            Add Ticket Page
-            <div className="add-ticket-page-table">
-                <form>
-                    <label> Name:
-                        <input type="text" value={ticketName} name="Name" onChange={handleTicketName} /><br />
-                    </label>
-                    <label> Detail
-                        <input type="text" value={ticketDetails} name="Detail" onChange={handleTicketDetails} /><br />
-                    </label>
-                    <label>Priority:
-                        <select onChange={handleTicketPriority}> 
-                            <option value="High" selected> High </option>
-                            <option value="Medium"> Medium </option>
-                            <option value="Low"> Low </option>
-                        </select> <br />
-                    </label>
-                    <label> Status:
-                        <select onChange={handleTicketStatus}>
-                            <option value="Open" selected>Open</option>
-                            <option value="Assigned">Assigned</option>
-                            <option value="Solved">Solved</option>
-                        </select> < br />
-                    </label>
-                    <Link to={"/projects/" + id + "/edit"}>
-                        <button onClick={handleTicketSubmit}>Create Ticket</button>
-                    </Link>
-                </form>
+        <div className={styles.addTicketContainer}>
+            <div className={styles.addTicketPage}>
+                <h4>Add Ticket Page</h4>
+                <div className="add-ticket-page-table">
+                    <form>
+                        <label> Name:
+                            <input type="text" value={ticketName} name="Name" onChange={handleTicketName} /><br />
+                        </label>
+                        <label> Detail
+                            <input type="text" value={ticketDetails} name="Detail" onChange={handleTicketDetails} /><br />
+                        </label>
+                        <label>Priority:
+                            <select onChange={handleTicketPriority}> 
+                                <option value="High" selected> High </option>
+                                <option value="Medium"> Medium </option>
+                                <option value="Low"> Low </option>
+                            </select> <br />
+                        </label>
+                        <label> Status:
+                            <select onChange={handleTicketStatus}>
+                                <option value="Open" selected>Open</option>
+                                <option value="Assigned">Assigned</option>
+                                <option value="Solved">Solved</option>
+                            </select> < br />
+                        </label>
+                        <Link to={"/projects/" + id + "/edit"}>
+                            <button onClick={handleTicketSubmit}>Create Ticket</button>
+                        </Link>
+                    </form>
+                </div>
             </div>
         </div>
     )

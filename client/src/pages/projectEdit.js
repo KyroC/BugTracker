e83f691f3f108d6a5d7985a7fc6ae4991df9f2e3
KyroC <Kyroeth@gmail.com>
2023-08-20 17:50:53 +0800

    wrapped previous div in container to enable better styling

--- client/src/pages/projectEdit.js
+++ client/src/pages/projectEdit.js
@@ -39,67 +39,69 @@ const ProjectEdit = () => {
     },[id])
     
     return(
-        <div>
-            <div className="project-edit-title"><h3>Edit Project</h3></div>
-            <div>
-                <div><b>Assign Users</b></div>
-                    <RoleForm userArray={userArray} projectArray={projectArray} setProjectArray={setProjectArray} projectId={id} projects={projects}/> <br />
-                <div><b>Current Assigned</b></div>
-                <div className="assigned-user-details-wrapper">
-                    <div className="assigned-user-details">
-                        <div className="assigned-user-header"><b>Name</b></div>
-                        <div className="assigned-user-header"><b>Email</b></div>
-                        <div className="assigned-user-header"><b>Role</b></div>
-                        <div className="assigned-user-header"><b>Delete</b></div>
-                    </div>
-                    {projectArray.users?.map((user)=> ( 
+        <div className="project-edit-container">
+            <div className="project-edit">
+                <div className="project-edit-title"><h3>Edit Project</h3></div>
+                <div>
+                    <div><b>Assign Users</b></div>
+                        <RoleForm userArray={userArray} projectArray={projectArray} setProjectArray={setProjectArray} projectId={id} projects={projects}/> <br />
+                    <div><b>Current Assigned</b></div>
+                    <div className="assigned-user-details-wrapper">
                         <div className="assigned-user-details">
-                            <div className="assigned-user-details-data">{user.name}</div>
-                            <div className="assigned-user-details-data">{user.email}</div>
-                            <div className="assigned-user-details-data">{user.role}</div>
-                            <div 
-                            className="assigned-user-details-delete">
-                                <button value={user.id} onClick={handleDelete}>Delete</button>
-                            </div>
+                            <div className="assigned-user-header"><b>Name</b></div>
+                            <div className="assigned-user-header"><b>Email</b></div>
+                            <div className="assigned-user-header"><b>Role</b></div>
+                            <div className="assigned-user-header"><b>Delete</b></div>
                         </div>
-                    ))}
-                 </div><br />
-                 <div className="Current Tickets">
-                    <div><b>Current Tickets</b></div>
-                    <div>
-                        <Link to={"/projects/" + id + "/addTicket"}>
-                            <button>Add tickets</button>
-                        </Link>
-                    </div>
-                    <div className="ticket-table-wrapper">
-                        <div className="ticket-data">
-                                <div className="ticket-data-header"><b>Ticket</b></div>
-                                <div className="ticket-data-header"><b>Detail</b></div>
-                                <div className="ticket-data-header"><b>Status</b></div>
-                                <div className="ticket-data-header"><b>Priority</b></div>
-                                <div className="ticket-data-header"><b>Actions</b></div>
-                            </div>
-                        {projectArray.bugs?.toReversed().map((bug) => (
-                            <div className="ticket-data">
-                                <div className="ticket-name">{bug.name}</div>
-                                <div className="ticket-detail">{bug.detail}</div>
-                                <div className="ticket-status">{bug.status}</div>
-                                <div className="ticket-priority">{bug.priority}</div>
-                                <div className="ticket-actions">
-                                    <Link to={"/bug/" + bug._id}>
-                                        <button>Edit Ticket</button>
-                                    </Link>
+                        {projectArray.users?.map((user)=> ( 
+                            <div className="assigned-user-details">
+                                <div className="assigned-user-details-data">{user.name}</div>
+                                <div className="assigned-user-details-data">{user.email}</div>
+                                <div className="assigned-user-details-data">{user.role}</div>
+                                <div 
+                                className="assigned-user-details-delete">
+                                    <button value={user.id} onClick={handleDelete}>Delete</button>
                                 </div>
                             </div>
                         ))}
+                    </div><br />
+                    <div className="Current Tickets">
+                        <div><b>Current Tickets</b></div>
+                        <div>
+                            <Link to={"/projects/" + id + "/addTicket"}>
+                                <button>Add tickets</button>
+                            </Link>
+                        </div>
+                        <div className="ticket-table-wrapper">
+                            <div className="ticket-data">
+                                    <div className="ticket-data-header"><b>Ticket</b></div>
+                                    <div className="ticket-data-header"><b>Detail</b></div>
+                                    <div className="ticket-data-header"><b>Status</b></div>
+                                    <div className="ticket-data-header"><b>Priority</b></div>
+                                    <div className="ticket-data-header"><b>Actions</b></div>
+                                </div>
+                            {projectArray.bugs?.toReversed().map((bug) => (
+                                <div className="ticket-data">
+                                    <div className="ticket-name">{bug.name}</div>
+                                    <div className="ticket-detail">{bug.detail}</div>
+                                    <div className="ticket-status">{bug.status}</div>
+                                    <div className="ticket-priority">{bug.priority}</div>
+                                    <div className="ticket-actions">
+                                        <Link to={"/bug/" + bug._id}>
+                                            <button>Edit Ticket</button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                 </div>
-            </div>
-            <div>
-                    <Link to="/projects">
-                        <button onClick={handleProjectDelete}>Delete Project</button>
-                    </Link>
                 </div>
+                <div>
+                        <Link to="/projects">
+                            <button onClick={handleProjectDelete}>Delete Project</button>
+                        </Link>
+                    </div>
+            </div>
         </div>
     )
 }

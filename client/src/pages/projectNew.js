7b14f34947cca3a4f280a4ff8e6656b10f458b14
KyroC <Kyroeth@gmail.com>
2023-08-20 17:52:24 +0800

    imported css module + wrapped div in new container for better styling
    imported projectNew css module for styling and added wrapped div in a new container for better styling

--- client/src/pages/projectNew.js
+++ client/src/pages/projectNew.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import projectService from '../services/projectService'
+import styles from './projectNew.module.css'
 
 let users = () => {
     return projectService.getUser()
@@ -41,33 +42,35 @@ const ProjectNew = () => {
         }
     
         return(
-            <div>
-                <h1>Create new project</h1>
-                <form>
-                    <label>
-                        Project Name:
-                        <input type="text" name="name" value={projectName} onChange={handleProjectName} /> <br />
-                    </label>
-                    <label>
-                        Project Details:
-                        <input type="text" name="details" value={projectDetails} onChange={handleProjectDetails} /> < br />
-                    </label>
-                    <label>
-                        Assign Users:
-                        <select value={projectUsers} onChange={handleProjectUsers} required size="3">
-                            {userArray?.map((user) => (
-                                <option value={user.id}>
-                                    {user.name}
-                                </option>
-                            ))}
-                        </select>
-                    </label>
-                    <Link to="/projects">
-                        <button onClick={handleProjectSubmit}>
-                            Create Project
-                        </button>
-                    </Link>
-                </form>
+            <div className={styles.projectNewContainer}>
+                <div className={styles.projectNew}>
+                    <h4>Create new project</h4>
+                    <form>
+                        <label>
+                            Project Name:
+                            <input type="text" name="name" value={projectName} onChange={handleProjectName} /> <br />
+                        </label>
+                        <label>
+                            Project Details:
+                            <input type="text" name="details" value={projectDetails} onChange={handleProjectDetails} /> < br />
+                        </label>
+                        <label>
+                            Assign Users:
+                            <select value={projectUsers} onChange={handleProjectUsers} required size="3">
+                                {userArray?.map((user) => (
+                                    <option value={user.id}>
+                                        {user.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </label>
+                        <Link to="/projects">
+                            <button onClick={handleProjectSubmit}>
+                                Create Project
+                            </button>
+                        </Link>
+                    </form>
+                </div>
             </div>
         )
 }
